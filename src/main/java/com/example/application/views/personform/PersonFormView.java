dd1460d89a015eb8311997860198a9ff89c408bc
start.vaadin.com <no-reply@vaadin.com>
2024-02-02 04:52:38 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/personform/PersonFormView.java
@@ -0,0 +1,86 @@
+package com.example.application.views.personform;
+
+import com.example.application.data.SamplePerson;
+import com.example.application.services.SamplePersonService;
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.spring.data.VaadinSpringDataHelpers;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+
+@PageTitle("Person Form")
+@Route(value = "person-form", layout = MainLayout.class)
+@Uses(Icon.class)
+public class PersonFormView extends Composite<VerticalLayout> {
+
+    public PersonFormView() {
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        H3 h3 = new H3();
+        FormLayout formLayout2Col = new FormLayout();
+        TextField textField = new TextField();
+        TextField textField2 = new TextField();
+        TextField textField3 = new TextField();
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        Button buttonPrimary = new Button();
+        Button buttonSecondary = new Button();
+        Grid basicGrid = new Grid(SamplePerson.class);
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        getContent().setJustifyContentMode(JustifyContentMode.START);
+        getContent().setAlignItems(Alignment.START);
+        layoutColumn2.setWidth("100%");
+        layoutColumn2.setMaxWidth("800px");
+        layoutColumn2.setHeight("min-content");
+        h3.setText("Información Personal");
+        h3.setWidth("100%");
+        formLayout2Col.setWidth("100%");
+        textField.setLabel("nombre");
+        textField2.setLabel("apellido");
+        textField3.setLabel("edad");
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.getStyle().set("flex-grow", "1");
+        buttonPrimary.setText("Save");
+        buttonPrimary.setWidth("min-content");
+        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        buttonSecondary.setText("Cancel");
+        buttonSecondary.setWidth("min-content");
+        basicGrid.setWidth("100%");
+        basicGrid.getStyle().set("flex-grow", "0");
+        setGridSampleData(basicGrid);
+        getContent().add(layoutColumn2);
+        layoutColumn2.add(h3);
+        layoutColumn2.add(formLayout2Col);
+        formLayout2Col.add(textField);
+        formLayout2Col.add(textField2);
+        formLayout2Col.add(textField3);
+        layoutColumn2.add(layoutRow);
+        layoutRow.add(buttonPrimary);
+        layoutRow.add(buttonSecondary);
+        getContent().add(basicGrid);
+    }
+
+    private void setGridSampleData(Grid grid) {
+        grid.setItems(query -> samplePersonService.list(
+                PageRequest.of(query.getPage(), query.getPageSize(), VaadinSpringDataHelpers.toSpringDataSort(query)))
+                .stream());
+    }
+
+    @Autowired()
+    private SamplePersonService samplePersonService;
+}
